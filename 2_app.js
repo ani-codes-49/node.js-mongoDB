633e35e04270b61166ee5a61cc39591ae484bcf8
anikarekar <aniruddhkarekar.1@gmail.com>
2024-05-23 23:19:04 +0530

    update

---
+++ 2_app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fileSystem = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write(
+            '<html><head><title>Node.js page</title></head><body><form action = "/message" method = "POST"><input type = "text" name = "msg"></input><button type = "submit">Submit</button></form></body></html>'
+        );
+        res.end();
+        return res;
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        //when data transmission is over
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = 'received text:' + parsedBody.split('=')[1];
+            fileSystem.writeFile('newFile.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+                return res;
+            });
+        });
+
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(
+        '<html><head><title>Node.js page</title></head><body>Hello from the Node.js server !</body></html>'
+    );
+
+    res.end();
+
+});
+
+server.listen(8080);
